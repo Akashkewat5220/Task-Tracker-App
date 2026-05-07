8e5fdf0ac8f65315a897c901f8adb45aeb79ffc1
Akashkewat5220 <kewatakash42@gmail.com>
2026-05-07 06:32:29 +0530

    Fix CSS loading issue: enable DEBUG, update template with static files and HTML structure

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0jcw)f!h&z90apa5c*k5*($)$w_17j3t+e3)2q020@0dz^m=5a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
